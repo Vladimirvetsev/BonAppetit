14a4e315cad0eb8b41c7c67ec122df5dca456825
Vladimir Vetsev <vladimrvetsev@gmail.com>
2017-09-16 09:58:01 +0300

    Pop-up forms are made(should be improved later on

---
+++ assets/javascript/demo.js
@@ -0,0 +1,12 @@
+function showLogInForm(){
+    document.getElementById("login-form").style.display='block';
+}
+function hideLogInForm(){
+    document.getElementById("login-form").style.display='none';
+}
+function showRegForm(){
+    document.getElementById("reg-form").style.display='block';
+}
+function hideRegForm(){
+    document.getElementById("reg-form").style.display='none';
+}
