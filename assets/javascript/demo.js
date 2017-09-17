73c0c7eedbd3866dcd35eed31d39b5cd2d2105ba
Vladimir Vetsev <vladimrvetsev@gmail.com>
2017-09-17 11:40:06 +0300

    Functional slideshow.

--- assets/javascript/demo.js
+++ assets/javascript/demo.js
@@ -10,3 +10,19 @@ function showRegForm(){
 function hideRegForm(){
     document.getElementById("reg-form").style.display='none';
 }
+
+    var slideNum=0;
+function slideShow(){
+    var slide=document.getElementsByClassName('slide');
+    for(var i=0; i<slide.length; i++){
+        slide[i].style.display='none';
+    if(slideNum>slide.length){
+        slideNum=1;
+    }
+    slide[slideNum-1].style.display='block'
+    }
+    slideNum++;
+   
+    setTimeout(slideShow,3000);
+}
+slideShow();
